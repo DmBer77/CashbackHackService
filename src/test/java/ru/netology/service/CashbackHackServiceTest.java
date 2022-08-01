8ae39b7d23ff8d9466253baaf19f4137c0c4b080
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-08-01 14:57:19 +0300

    add tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,10 +6,32 @@ import org.junit.jupiter.api.Test;
 class CashbackHackServiceTest {
 
     @Test
-    void shouldReturnTheMissingSumForBonus() {
+    void shouldReturnTheMissingAmountForBonus() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int sum = 1900;
         int expected = 100;
+        int actual = cashbackHackService.remain(sum);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldReturnTheMissingAmountForBonusIfTheActualAmountLessThanOneThousand() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int sum = 750;
+        int expected = 250;
+        int actual = cashbackHackService.remain(sum);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldReturnTheMissingAmountForBonusIfTheActualAmountEqualsTheExpected() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int sum = 2000;
+        int expected = 1000;
         int actual = cashbackHackService.remain(sum);
 
         Assertions.assertEquals(expected, actual);

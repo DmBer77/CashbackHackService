db1e76fe3fc4f51dd52883f4c044caedfa7202dc
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-08-01 16:55:54 +0300

    change for run JUnit Jupiter and JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,11 +2,12 @@ package ru.netology.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldReturnTheMissingAmountForBonus() {
+    public void shouldReturnTheMissingAmountForBonusForJunitFour() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int sum = 1900;
         int expected = 100;
@@ -17,7 +18,18 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldReturnTheMissingAmountForBonusIfTheActualAmountLessThanOneThousand() {
+    public void shouldReturnTheMissingAmountForBonusForJUnitJupiter() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int sum = 1900;
+        int expected = 100;
+        int actual = cashbackHackService.remain(sum);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldReturnTheMissingAmountForBonusIfTheActualAmountLessThanOneThousandForJUnitFour() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int sum = 750;
         int expected = 250;
@@ -28,13 +40,35 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldReturnTheMissingAmountForBonusIfTheActualAmountEqualsTheExpected() {
+    public void shouldReturnTheMissingAmountForBonusIfTheActualAmountLessThanOneThousandForJunitJupiter() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int sum = 750;
+        int expected = 250;
+        int actual = cashbackHackService.remain(sum);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldReturnTheMissingAmountForBonusIfTheActualAmountEqualsTheExpectedForJUnitFour() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int sum = 2000;
         int expected = 0;
         int actual = cashbackHackService.remain(sum);
 
         Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldReturnTheMissingAmountForBonusIfTheActualAmountEqualsTheExpectedForJUnitJupiter() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int sum = 2000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(sum);
+
+        Assertions.assertEquals(expected, actual);
 
     }
 }
